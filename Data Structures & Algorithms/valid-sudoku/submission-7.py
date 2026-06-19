b80712608c1d69141c1cf9c897ce33c74b054c5d
Anna Laura Taormina <annalaurataormina@gmail.com>
2026-06-19 13:06:15 +0100

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    
+        seen_cols = []
+        seen_boxes = []
+
+        for i in range(9):
+            seen_cols.append(defaultdict(int))
+            seen_boxes.append(defaultdict(int))
+        
+        for i, r in enumerate(board):
+
+            seen = defaultdict(int)
+
+            box = i // 3 * 3
+
+            for j, n in enumerate(r):
+
+                if n == '.':
+                    continue
+                
+                seen_cols[j][n] += 1
+
+                seen_boxes[box + j // 3][n] += 1
+
+                seen[n] += 1
+
+            if sum(seen.values()) != len(seen.values()):
+                return False
+
+        for seen_col in seen_cols:
+            if sum(seen_col.values()) != len(seen_col.values()):
+                return False
+
+        print(seen_boxes)
+
+        for seen_box in seen_boxes:
+            if sum(seen_box.values()) != len(seen_box.values()):
+                return False
+
+        return True
+
+        
+
+        
+            
+
+
+
+
+
+        
