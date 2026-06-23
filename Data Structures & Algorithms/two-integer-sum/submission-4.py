a43c19519d8e970011cc0bd36f59b6396cb3dbd8
Anna Laura Taormina <annalaurataormina@gmail.com>
2026-06-23 11:34:03 +0100

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        occurrences = {}
+
+        for i, n in enumerate(nums):
+            
+            diff = target - n
+
+            if diff in occurrences:
+                return [occurrences[diff], i]
+
+            occurrences[n] = i
+
+        
